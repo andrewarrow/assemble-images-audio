6034b18c454d7144d8bf29c9e8c6ede8bed5af63
Andrew Arrow <oneone@gmail.com>
2020-09-30 14:06:52 -0700

    seconds per frame

--- cat_images.sh
+++ cat_images.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-rm video/*
-ffmpeg -framerate 1 -pattern_type glob -i '../aae1/*.png' -c:v libx264 -r 30 -pix_fmt yuv420p video/out.mp4
+ffmpeg -framerate 1 -pattern_type glob -i 'video/*.png' -c:v libx264 -r 30 -pix_fmt yuv420p video/out.mp4
 
 ffmpeg -i video/out.mp4 -i ../aae1/audio/001.m4a -c copy -map 0:v:0 -map 1:a:0 video/final.mov
--- main.go
+++ main.go
@@ -16,40 +16,52 @@ func main() {
 	}
 	dir := os.Args[1] + "/images"
 	files, _ := ioutil.ReadDir(dir)
-	m := map[string][]int{}
 	for _, f := range files {
 		name := f.Name()
 		if strings.HasSuffix(name, ".png") {
 			three := name[0:3]
-			min := name[4:6]
+			order := name[4:6]
+			seconds := name[7:9]
 
-			mini, _ := strconv.Atoi(min)
-			m[three] = append(m[three], mini)
+			fmt.Println(three, order, seconds)
+			secondsi, _ := strconv.Atoi(seconds)
+
+			for i := 0; i < secondsi; i++ {
+				doCopy(dir, three, order, seconds, i)
+			}
 		}
 	}
-	for i := 0; i < len(m); i++ {
-		key := fmt.Sprintf("%03d", i+1)
-		size := len(m[key])
-		mapOfInts := map[int]bool{}
-		for _, v := range m[key] {
-			mapOfInts[v] = true
-		}
-		for i, v := range m[key] {
-			if i == size-1 {
-			} else {
-				vstart := v
-				for {
-					exec.Command("cp", fmt.Sprintf("%s/%s_%02d.png", dir, key, v),
-						fmt.Sprintf("video/%s_%02d.png", key, vstart)).Run()
-					vstart++
-					if mapOfInts[vstart] == false {
-						exec.Command("cp", fmt.Sprintf("%s/%s_%02d.png", dir, key, v),
-							fmt.Sprintf("video/%s_%02d.png", key, vstart)).Run()
-					} else {
-						break
+	/*
+		for i := 0; i < len(m); i++ {
+			key := fmt.Sprintf("%03d", i+1)
+			size := len(m[key])
+			mapOfInts := map[int]bool{}
+			for _, v := range m[key] {
+				mapOfInts[v] = true
+			}
+			for i, v := range m[key] {
+				if i == size-1 {
+					for j := 0; j < 5; j++ {
+						doCopy(dir, key, v, v+j)
+					}
+				} else {
+					vstart := v
+					for {
+						doCopy(dir, key, v, vstart)
+						vstart++
+						if mapOfInts[vstart] == false {
+							doCopy(dir, key, v, vstart)
+						} else {
+							break
+						}
 					}
 				}
 			}
 		}
-	}
+	*/
+}
+
+func doCopy(dir, key, order, seconds string, vstart int) {
+	exec.Command("cp", fmt.Sprintf("%s/%s_%s-%s.png", dir, key, order, seconds),
+		fmt.Sprintf("video/%s_%02d.png", key, vstart)).Run()
 }
